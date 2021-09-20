5d0ba10e3fb07695010bc04db5d24020b2cb588b
Martin B <mart.baj2005@gmail.com>
2021-09-20 11:16:14 +1000

    pushable amethyst

---
+++ src/main/java/github/elmartino4/mechanicalFactory/mixin/BuddingAmethystBlockMixin.java
@@ -0,0 +1,17 @@
+package github.elmartino4.mechanicalFactory.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BuddingAmethystBlock;
+import net.minecraft.block.piston.PistonBehavior;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BuddingAmethystBlock.class)
+public class BuddingAmethystBlockMixin {
+    @Inject(at = @At("HEAD"), method = "getPistonBehavior(Lnet/minecraft/block/BlockState;)Lnet/minecraft/block/piston/PistonBehavior;", cancellable = true)
+    void getPistonBehavior(BlockState state, CallbackInfoReturnable<PistonBehavior> cir) {
+        cir.setReturnValue(PistonBehavior.NORMAL);
+    }
+}
